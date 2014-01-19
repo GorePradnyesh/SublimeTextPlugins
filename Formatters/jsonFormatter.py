bbe591864372fcd6b1827db57749f9ba479016cc
Pradnyesh Gore <pgore@adobe.com>
2014-01-19 12:15:21 -0800

    jsonFormatter timestamp: message stacktrace

---
+++ Formatters/jsonFormatter.py
@@ -0,0 +1,20 @@
+import sublime, sublime_plugin
+import json
+
+class JsonformatCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		selection = self.view.sel()
+		for region in selection:
+			region_text = self.view.substr(region)
+			lines = region_text.splitlines()
+
+			newlines= []
+			for line in lines:
+				jsonLine = json.loads(line)
+				if 'stacktrace' in jsonLine.keys():
+					content = jsonLine['timestamp']+": "+jsonLine['message'] + ", STACKTRACE:"+ jsonLine['stacktrace']+ "\n"
+				else:
+					content = jsonLine['timestamp']+" : "+jsonLine['message'] + "\n"
+				newlines.append(content)
+			replaced_string  = ''.join(newlines)
+			self.view.replace(edit, region, replaced_string)
